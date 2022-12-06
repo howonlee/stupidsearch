8b36b37b6fc87c8d5b4e3b3e98be4a9f16175942
Howon Lee <hlee.howon@gmail.com>
2022-12-05 16:05:24 -0800

    put in ngrams

--- ngrams.py
+++ ngrams.py
@@ -4,21 +4,23 @@ import numpy.linalg as npl
 import string
 import sys
 import re
+import itertools
 import operator
 
 def normalized_uniform():
     res = npr.uniform(size=100)
     return res / npl.norm(res)
 
-def word_to_vec(word, alphabet):
+def word_to_ngram_vec(word, ngrams, n):
     letters = list(word)
+    word_ngrams = map(lambda x: "".join(x), zip(*[letters[i:] for i in range(n)]))
     res = np.zeros(100)
-    for letter in letters:
-        res += alphabet[letter]
+    for curr_ngram in word_ngrams:
+        res += ngrams[curr_ngram]
     return res / npl.norm(res)
 
-def query(q, corpus_vecs, alph_vecs):
-    q_vec = word_to_vec(q, alph_vecs)
+def query(q, corpus_vecs, ngram_vecs, n):
+    q_vec = word_to_ngram_vec(q, ngram_vecs, n)
     res = []
     for key, vec in corpus_vecs.items():
         # normally we need to divide by npl norm but those are uniformly 1 because we made it so...
@@ -28,16 +30,16 @@ def query(q, corpus_vecs, alph_vecs):
     return list(sorted(res, key=operator.itemgetter(1), reverse=True))
 
 if __name__ == "__main__":
+    n = 2
     with open("corpus.txt", "r") as corpus_file:
         unprocessed = corpus_file.read()
         no_unicode = re.sub(r'[^\x00-\x7f]', "", unprocessed)
         corpus = set(no_unicode.lower().split())
         alphabet = list(string.printable)
-        alph_vecs = {letter: normalized_uniform() for letter in alphabet}
-        corpus_vecs = {word: word_to_vec(word, alph_vecs) for word in corpus}
-        print(len(corpus))
+        ngram_vecs = {"".join(ngram): normalized_uniform() for ngram in itertools.product(alphabet, repeat=2)}
+        corpus_vecs = {word: word_to_ngram_vec(word, ngram_vecs, n) for word in corpus}
         while True:
             q = input("Query: ")
-            print("\n".join(map(str, query(q, corpus_vecs, alph_vecs)[:5])))
+            print("\n".join(map(str, query(q, corpus_vecs, ngram_vecs, n)[:5])))
             if q == "quit":
                 sys.exit(0)
